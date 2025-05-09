a2a11d691a16df0e92dd0c6d0ab41e91be677a43
min-38 <mindino03@gmail.com>
2025-05-09 23:41:38 +0900

    김민서-진입화면 처리

---
+++ Omok_Client/Login.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Omok_Client
+{
+    partial class Login
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login_id = new MetroFramework.Controls.MetroTextBox();
+            this.login_btn = new MetroFramework.Controls.MetroButton();
+            this.signup_btn = new MetroFramework.Controls.MetroButton();
+            this.exit_btn = new MetroFramework.Controls.MetroButton();
+            this.login_pw = new MetroFramework.Controls.MetroTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // login_id
+            // 
+            this.login_id.Location = new System.Drawing.Point(141, 185);
+            this.login_id.Name = "login_id";
+            this.login_id.Size = new System.Drawing.Size(283, 40);
+            this.login_id.TabIndex = 0;
+            this.login_id.TextChanged += new System.EventHandler(this.login_TextChanged);
+            // 
+            // login_btn
+            // 
+            this.login_btn.Location = new System.Drawing.Point(471, 185);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(191, 111);
+            this.login_btn.TabIndex = 1;
+            this.login_btn.Text = "로그인";
+            this.login_btn.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            // 
+            // signup_btn
+            // 
+            this.signup_btn.Location = new System.Drawing.Point(141, 349);
+            this.signup_btn.Name = "signup_btn";
+            this.signup_btn.Size = new System.Drawing.Size(191, 78);
+            this.signup_btn.TabIndex = 2;
+            this.signup_btn.Text = "회원가입";
+            this.signup_btn.Click += new System.EventHandler(this.signup_btn_Click);
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.Location = new System.Drawing.Point(471, 349);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(191, 78);
+            this.exit_btn.TabIndex = 3;
+            this.exit_btn.Text = "나가기";
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_Click);
+            // 
+            // login_pw
+            // 
+            this.login_pw.Location = new System.Drawing.Point(141, 256);
+            this.login_pw.Name = "login_pw";
+            this.login_pw.Size = new System.Drawing.Size(283, 40);
+            this.login_pw.TabIndex = 5;
+            this.login_pw.TextChanged += new System.EventHandler(this.login_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(247, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 86);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "오목게임";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 520);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.login_pw);
+            this.Controls.Add(this.exit_btn);
+            this.Controls.Add(this.signup_btn);
+            this.Controls.Add(this.login_btn);
+            this.Controls.Add(this.login_id);
+            this.Name = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox login_id;
+        private MetroFramework.Controls.MetroButton login_btn;
+        private MetroFramework.Controls.MetroButton signup_btn;
+        private MetroFramework.Controls.MetroButton exit_btn;
+        private MetroFramework.Controls.MetroTextBox login_pw;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Omok_Client/SignUpForm.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+using MetroFramework.Forms;
+
+namespace Omok
+{
+    public partial class SignUpForm : MetroForm
+    {
+        private bool nickname_dupl_checked = false;
+        private bool id_dupl_checked = false;
+        private bool pw_checked = false;
+        private bool pw2_checked = false;
+
+        string userDBPath = Path.GetFullPath(Path.Combine("..", "..", "..", "db", "user.txt"));
+
+        public SignUpForm()
+        {
+            InitializeComponent();
+
+            signup_pw.UseSystemPasswordChar = true;
+            signup_pw2.UseSystemPasswordChar = true;
+        }
+
+        private void SignUpForm_Load(object sender, EventArgs e)
+        {
+            signup_btn.Enabled = false;
+            nn_dupl_chk_btn.Enabled = false;
+            id_dupl_chk_btn.Enabled = false;
+        }
+
+        private void nn_dupl_chk_btn_Click(object sender, EventArgs e)
+        {
+            // TODO: 닉네임 중복 체크 로직 추가
+            if (!File.Exists(userDBPath))
+            {
+                MessageBox.Show("회원가입 > 닉네임 중복 체크 > DB 오류");
+                return;
+            }
+
+            bool found = false;
+
+            foreach (string line in File.ReadLines(userDBPath))
+            {
+                string[] tokens = line.Split(',');
+                if (tokens.Length >= 2)
+                {
+                    string nickname = tokens[1];
+                    if (signup_name.Text == nickname)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                MessageBox.Show("사용 가능한 닉네임입니다.");
+                nickname_dupl_checked = true;
+            }
+            else
+                MessageBox.Show("이미 존재하는 닉네임입니다.");
+
+            validateForm();
+        }
+
+        private void id_dupl_chk_btn_Click(object sender, EventArgs e)
+        {
+            // TODO: 아이디 중복 체크 로직 추가
+            if (!File.Exists(userDBPath))
+            {
+                MessageBox.Show("회원가입 > 아이디 중복 체크 > DB 오류");
+                return;
+            }
+
+            bool found = false;
+
+            foreach (string line in File.ReadLines(userDBPath))
+            {
+                string[] tokens = line.Split(',');
+                if (tokens.Length >= 2)
+                {
+                    string id = tokens[2];
+                    if (signup_id.Text == id)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                MessageBox.Show("사용 가능한 유저명입니다.");
+                id_dupl_checked = true;
+            } 
+            else
+                MessageBox.Show("이미 존재하는 유저명입니다.");
+
+            validateForm();
+        }
+
+        private void signup_name_TextChanged(object sender, EventArgs e)
+        {
+            nickname_dupl_checked = false;
+            nn_dupl_chk_btn.Enabled = false;
+            if (signup_name.Text.Length > 0)
+                nn_dupl_chk_btn.Enabled = true;
+
+            validateForm();
+        }
+
+        private void signup_id_TextChanged(object sender, EventArgs e)
+        {
+            id_dupl_checked = false;
+            id_dupl_chk_btn.Enabled = false;
+            if (signup_id.Text.Length > 0)
+                id_dupl_chk_btn.Enabled = true;
+            
+            validateForm();
+        }
+
+        private void signup_pw_TextChange(object sender, EventArgs e)
+        {
+            if (signup_pw.Text.Length < 4 || signup_pw.Text.Length > 10)
+            {
+                signup_pw_warning_lb.Text = "비밀번호는 4 ~ 10자 이내로 작성해야 합니다.";
+                signup_pw_warning_lb.ForeColor = Color.Red;
+                pw_checked = false;
+            }
+            else
+            {
+                signup_pw_warning_lb.Text = "이 비밀번호는 사용 가능합니다.";
+                signup_pw_warning_lb.ForeColor = Color.Green;
+                pw_checked = true;
+            }
+            signup_pw2_TextChange(sender, null);
+            validateForm();
+        }
+
+        private void signup_pw2_TextChange(object sender, EventArgs e)
+        {
+            if (signup_pw.Text == "")
+                return;
+
+            if (signup_pw.Text != signup_pw2.Text)
+            {
+                signup_pw2_warning_lb.Text = "비밀번호가 일치하지 않습니다.";
+                signup_pw2_warning_lb.ForeColor = Color.Red;
+                pw2_checked = false;
+            }
+            else
+            {
+                signup_pw2_warning_lb.Text = "비밀번호가 일치합니다.";
+                signup_pw2_warning_lb.ForeColor = Color.Green;
+                pw2_checked = true;
+            }
+            validateForm();
+        }
+
+        private void validateForm()
+        {
+            if (nickname_dupl_checked && id_dupl_checked && pw_checked && pw2_checked)
+            {
+                signup_btn.Enabled = true;
+            }
+            else
+            {
+                signup_btn.Enabled = false;
+            }
+        }
+
+        private void signup_btn_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(userDBPath))
+            {
+                MessageBox.Show("회원가입 > 회원가입 > DB 오류");
+                return;
+            }
+
+            string content = File.ReadAllText(userDBPath);
+            bool needsLineBreak = !string.IsNullOrEmpty(content) &&
+                                  !content.EndsWith("\n") && !content.EndsWith("\r");
+
+            int lineNumber = File.ReadAllLines(userDBPath).Length + 1;
+
+            using (StreamWriter sw = new StreamWriter(userDBPath, true))
+            {
+                if (needsLineBreak)
+                    sw.WriteLine();  // 단순 개행 한 번
+
+                sw.WriteLine($"{lineNumber},{signup_name.Text},{signup_id.Text},{signup_pw.Text}");  // 개행 자동 포함
+            }
+
+            MessageBox.Show("회원가입되었습니다.");
+            this.Close();
+        }
+
+
+        private void close_btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
